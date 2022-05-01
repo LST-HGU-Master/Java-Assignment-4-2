54bc08dada5522cd9437e36c2fc2f954bc02454a
YoshiHirat <50605381+YoshiHirat@users.noreply.github.com>
2022-05-01 14:55:08 +0900

    modification

--- src/test/java/Prog42Test.java
+++ src/test/java/Prog42Test.java
@@ -1,51 +1,77 @@
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterEach;
 import java.io.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class Prog42Test {
+    InputStream originalIn;
+    PrintStream originalOut;
+    ByteArrayOutputStream bos;
+    StandardInputStream in;
+
+    @BeforeEach
+    void before() {
+        //back up binding
+        originalIn  = System.in;
+        originalOut = System.out;
+        //modify binding
+        bos = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(bos));
+        
+        in = new StandardInputStream();
+        System.setIn(in);
+    }
+    
+    @AfterEach
+    void after() {
+       System.setOut(originalOut);
+       System.setIn(originalIn);
+    }
 
     @Test
     public void testManyNumbers()
     {
-        PrintStream originalOut = System.out;
-        ByteArrayOutputStream bos = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(bos));
-
         // action
         Prog42.main(new String[]{"1", "2", "3", "1", "2", "3", "1", "2", "3", "1", "2", "3", "1", "2", "3"});
 
         // assertion
-        assertEquals("1,2,3,1,2,3,1,2,3,1,2,3,1,2,3\n", bos.toString());
-
-        // undo the binding in System
-        System.setOut(originalOut);
+        try {
+            assertEquals("1,2,3,1,2,3,1,2,3,1,2,3,1,2,3" + System.lineSeparator(), bos.toString(),
+                        "print出力が期待通りになっていません!（コード内で繰り返し回数を直接数値指定しているかも？）"
+            );
+        } catch (AssertionError err) {
+            after();
+            throw err;
+        }
     }
 
     @Test
     public void testSingleNumber()
     {
-        PrintStream originalOut = System.out;
-        ByteArrayOutputStream bos = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(bos));
-
         // action
-        Prog42.main(new String[]{"1"});
-
+        try {
+            Prog42.main(new String[]{"1"});
+        } catch (ArrayIndexOutOfBoundsException expt) {
+            after();
+            AssertionError asErr = new AssertionError("実行時引数が１つの場合に、存在しないargs[1]の値を参照しようとしています!");
+            throw asErr;
+        }
         // assertion
-        assertEquals("1\n", bos.toString());
-
-        // undo the binding in System
-        System.setOut(originalOut);
+        try {
+            assertEquals("1" + System.lineSeparator(), bos.toString(), 
+                         "実行時引数が「１」の場合に、print出力が期待通りになっていません!"
+            );
+        } catch (AssertionError asErr) {
+            after();
+            throw asErr; 
+        }
     }
 
     @Test
     public void testNoInput()
     {
-        PrintStream originalOut = System.out;
-        ByteArrayOutputStream bos = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(bos));
-
         // assertion
         try {
             // action
@@ -56,8 +82,6 @@ public class Prog42Test {
             // pass
         }
 
-        // undo the binding in System
-        System.setOut(originalOut);
     }
 
 }
